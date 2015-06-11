69992026a30e06b2cac0709ab3a3b3bdbcb5aad1
Jeff Cann <cannjeff@gmail.com>
2015-06-11 10:13:39 -0400

    initial config

---
+++ app.js
@@ -0,0 +1,25 @@
+var http 		= require('http'),
+	httpProxy 	= require('http-proxy'),
+	proxy 		= httpProxy.createProxyServer({}),
+	url			= require('url'),
+	proxyConfig;
+
+http.createServer(function ( req, res ) {
+	var hostname = req.headers.host.split(':')[0],
+		pathname = url.parse(req.url).pathname;
+
+	console.log('hostname', hostname);
+	console.log('pathname', pathname);
+
+	switch (hostname) {
+		case 'cryptoquip.io':
+			proxy.web(req, res, { target: 'http://localhost:3000' });
+			break;
+		case 'api.cryptoquip.io':
+			proxy.web(req, res, { target: 'http://localhost:3001' });
+		default:
+			console.log('No app found for', hostname, pathname);
+	}
+}).listen(80, function () {
+	console.log('Listening on port 80');
+});
